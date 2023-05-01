178131ebedf612a8f0028d3e9beb6fb0ff5f7950
Moazam05 <salmanmoazam08@gmail.com>
2023-05-01 16:57:57 +0500

    login functionality

--- discord-backend/controllers/login.js
+++ discord-backend/controllers/login.js
@@ -1,5 +1,34 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+// Local Imports
+const User = require("../models/user");
+
 const login = async (req, res) => {
-	res.send("login route");
+	try {
+		const { email, password } = req.body;
+
+		console.log("req", req);
+
+		const user = await User.findOne({ email: email.toLowerCase() });
+
+		if (user && bcrypt.compare(password, user.password)) {
+			// send new token
+
+			const token = "JWT token";
+
+			return res.status(200).send({
+				userDetails: {
+					email: user.email,
+					token: token,
+					password: user.password,
+				},
+			});
+		}
+
+		return res.status(400).send("Invalid Credentials");
+	} catch (error) {
+		return res.status(500).send("Something went wrong. Please try again");
+	}
 };
 
 module.exports = login;
