2935638ff286556f16f511fea447bf1591eb2771
Moazam05 <salmanmoazam08@gmail.com>
2023-05-01 15:41:42 +0500

    joi validation

--- discord-backend/routes/authRoutes.js
+++ discord-backend/routes/authRoutes.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const router = express.Router();
-
 const authController = require("../controllers/authController");
 
-router.post("/register", authController.controllers.register);
+const Joi = require("joi");
+const validator = require("express-joi-validation").createValidator({});
 
-router.post("/login", authController.controllers.login);
+const registerSchema = Joi.object({
+	username: Joi.string().min(3).max(12).required(),
+	password: Joi.string().min(6).max(12).required(),
+	email: Joi.string().email().required(),
+});
+
+const loginSchema = Joi.object({
+	email: Joi.string().email().required(),
+	password: Joi.string().min(6).max(12).required(),
+});
+
+router.post(
+	"/register",
+	validator.body(registerSchema),
+	authController.controllers.register
+);
+router.post(
+	"/login",
+	validator.body(loginSchema),
+	authController.controllers.login
+);
 
 module.exports = router;
