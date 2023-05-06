3ff4210a7773180d0ebf031a488d2919719f43eb
Moazam05 <salmanmoazam08@gmail.com>
2023-05-06 13:32:53 +0500

    routes set

--- discord-frontend/src/App.js
+++ discord-frontend/src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./views/Login/Login.jsx";
+import Register from "./views/Register/Register.jsx";
+import Dashboard from "./views/Dashboard/Dashboard.jsx";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
