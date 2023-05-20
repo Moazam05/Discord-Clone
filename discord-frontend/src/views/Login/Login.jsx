4b001f36f84c5227a011e57ca783b457e183a010
Moazam05 <salmanmoazam08@gmail.com>
2023-05-20 13:08:06 +0500

    validation in login form

--- discord-frontend/src/views/Login/Login.jsx
+++ discord-frontend/src/views/Login/Login.jsx
@@ -1,15 +1,39 @@
 // React Imports
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // Custom
 import WhiteInputField from "../../shared/InputFields/WhiteInputField";
 import PrimaryButton from "../../shared/Buttons/PrimaryButton";
+import { Tooltip } from "@mui/material";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isFormValid, setIsFormValid] = useState(false);
 
-  const handleLogin = () => {};
+  const validateLoginForm = ({ email, password }) => {
+    const isMailValid = validateEmail(email);
+    const isPasswordValid = validatePassword(password);
+
+    return isMailValid && isPasswordValid;
+  };
+
+  const validateEmail = (email) => {
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(email);
+  };
+
+  const validatePassword = (password) => {
+    return password.length > 6 && password.length < 12;
+  };
+
+  useEffect(() => {
+    setIsFormValid(validateLoginForm({ email, password }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [email, password, setIsFormValid]);
+
+  const handleLogin = () => {
+    console.log(email, password);
+  };
 
   return (
     <>
@@ -28,13 +52,16 @@ const Login = () => {
         placeholder="Enter Password"
         type="password"
       />
-
-      <PrimaryButton
-        label="Log in"
-        additionalStyles={{ marginTop: "30px" }}
-        onClick={handleLogin}
-        disabled={!isFormValid}
-      />
+      <Tooltip title={!isFormValid ? "Please fill all fields" : ""}>
+        <div>
+          <PrimaryButton
+            label="Log in"
+            additionalStyles={{ marginTop: "30px" }}
+            onClick={handleLogin}
+            disabled={!isFormValid}
+          />
+        </div>
+      </Tooltip>
     </>
   );
 };
